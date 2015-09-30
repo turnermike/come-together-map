062b9f725d62bb876cb5e44a8370e44df671235b
Mike Turner <mturner@realinteractive.ca>
2015-09-30 12:03:10 -0400

    added new twitter api creds

--- ct_application/models/Site_model.php
+++ ct_application/models/Site_model.php
@@ -5,6 +5,15 @@ class Site_model extends CI_Model{
 
     function get_tweets_central(){
 
+        // load twitter library
+        $settings = array(
+            'oauth_access_token' => "48384543-mLPyQLMX5xNwv3bwWC4l3eG7I623dofZJgGFbQL1T",
+            'oauth_access_token_secret' => "5qsY1WU8iufKYQkaaCHDRGvbsNaR7mxeEniWCB84tZDg9",
+            'consumer_key' => "6eQOA1iexKYtkKSYnLSm9vJyj",
+            'consumer_secret' => "cyGAvdIAhg24nF4SiPZn3dN5YveMBd9Ma4q8R83zSNK6xkqfsQ"
+        );
+        $this->load->library('twitterapiexchange', $settings);
+
         return 'return stuff here';
 
     }
