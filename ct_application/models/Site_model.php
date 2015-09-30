60c1eb0dd8206cce51f09c53dae455f19ac52972
Mike Turner <mturner@realinteractive.ca>
2015-09-30 11:07:50 -0400

    going back

---
+++ ct_application/models/Site_model.php
@@ -0,0 +1,15 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Site_model extends CI_Model{
+
+    function get_tweets(){
+
+        return 'testing abc12k';
+
+
+
+
+    }
+
+}
