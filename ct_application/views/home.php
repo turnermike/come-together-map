b7df8efb62407eeffa3c47020a56b0f5bc5f56d0
Mike Turner <mturner@realinteractive.ca>
2015-10-06 09:13:20 -0400

    added GA

--- ct_application/views/home.php
+++ ct_application/views/home.php
@@ -54,5 +54,17 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <script src='https://api.mapbox.com/mapbox.js/plugins/leaflet-markercluster/v0.4.0/leaflet.markercluster.js'></script>
     <script src="library/js/app.js"></script>
 
+
+    <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+        ga('create', 'UA-68480188-1', 'auto');
+        ga('send', 'pageview');
+    </script>
+
+
 </body>
 </html>
